137c2342204cf8d08da629f9dcd2f0a1df517292
vahid mohammadi <v.mohammadi@sci.ui.ac.ir>
2017-10-03 22:15:18 +0330

    first commit

---
+++ test.py
@@ -0,0 +1,9 @@
+#ضرب تانسوری
+a = [[1,2],[1,2]]
+b = [[3,4],[3,4]]
+for i in range(2):
+    for j in range(2):
+        for m in range(2):
+            for n in range(2):
+                c=a[i][j]*b[m][n]
+prin(c)
